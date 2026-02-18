4f64f6d7babb53f36e41926c36a64a0148a12ec6
Metehan <metehanaltuntas9@gmail.com>
2026-02-18 16:51:43 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,22 @@
-// 1. Firebase kütüphanelerini internetten çekiyoruz (npm gerekmez)
+// firebase-config.js - GÜNCEL HALİ
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"; // Giriş sistemi eklendi
 
-// 2. SENİN RESİMDEKİ BİLGİLERİ BURAYA YAPIŞTIR
-// (Resimdeki const firebaseConfig = { ... } süslü parantez içindekileri buraya al)
+// SENİN BİLGİLERİN (Burası senin attığın resimdeki gibi kalsın)
 const firebaseConfig = {
-  apiKey: "AIzaSyAFQqlXvRGnet7vKJLwtbVTkHSn0_IBnBs", // Senin resimdeki kod
-  authDomain: "menu-25142.firebaseapp.com",        // Senin resimdeki kod
-  projectId: "menu-25142",                           // Senin resimdeki kod
-  storageBucket: "menu-25142.firebasestorage.app",  // Senin resimdeki kod
-  messagingSenderId: "679156264820",                 // Senin resimdeki kod
-  appId: "1:679156264820:web:e1d6ce6a01d0cb89370621", // Senin resimdeki kod
-  measurementId: "G-M0655LPDNB"                      // Senin resimdeki kod
+  apiKey: "AIzaSyAFQqlXvRGnet7vKJLwtbVTkHSn0_IBnBs",
+  authDomain: "menu-25142.firebaseapp.com",
+  projectId: "menu-25142",
+  storageBucket: "menu-25142.firebasestorage.app",
+  messagingSenderId: "679156264820",
+  appId: "1:679156264820:web:e1d6ce6a01d0cb89370621",
+  measurementId: "G-M0655LPDNB"
 };
 
-// 3. Firebase'i başlatıyoruz
+// Uygulamayı başlat
 const app = initializeApp(firebaseConfig);
 
-// 4. Veritabanını dışarıya açıyoruz (Diğer dosyalarda kullanmak için)
+// Dışarıya aktarılanlar (Admin sayfası bunları kullanıyor)
 export const db = getFirestore(app);
+export const auth = getAuth(app);  // İŞTE EKSİK OLAN SATIR BU
